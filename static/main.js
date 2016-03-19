e84572019cecb6419fa2081ecf1b90cbc0841f0d
Shane Reustle <shane@crath.com>
2016-03-19 17:09:46 +0900

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,3 +1,14 @@
 $('input[type=checkbox]').click(function(){
-  console.log($(this).parents('tr').attr('data-id'));
+  var parentTr = $(this).parents('tr');
+  var episode = parentTr.attr('data-id');
+  var watched = $(this).is(':checked');
+  
+  if(watched){
+    Session.set('watched-' + episode, true);
+    parentTr.addClass('table-success');
+  }else{
+    Session.set('watched-' + episode, undefined);
+    parentTr.removeClass('table-success');
+  }
+  
 });
