570330239fd8f74aa7207b83cf2d83bc1843c230
Francisco Presencia <public@francisco.io>
2019-12-22 07:34:42 +0100

    Moved to Drive DB

--- static/main.js
+++ static/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Google Sheet ID
+const SHEET = "17rkB73L_tcfhr4RugSekj07P8hVenTJu3b9Co_hPgQI";
+
 const YT_VID = 'https://www.youtube.com/watch?v='
 const YT_SEARCH = 'https://www.youtube.com/results?search_query=Japanology+'
 const YT_IMG = 'https://i.ytimg.com/vi/'
@@ -11,21 +14,21 @@ let DEFAULT_SEASON = 2019 // TODO Change to most recent
 
 function renderEpisode(season, episode) {
     // Render an individual episode into the grid
-    
+
     let url = ''
     let img = ''
-    if(episode.vid){
-        url = YT_VID + episode.vid
-        img = YT_IMG + episode.vid + YT_IMG_FILE
+    if(episode.video){
+        url = YT_VID + episode.video
+        img = YT_IMG + episode.video + YT_IMG_FILE
     }else{
         url = YT_SEARCH + encodeURIComponent(episode.title)
         img = '/static/missing-video.png'
     }
-    
+
     let el = document.createElement('div')
     el.className += 'col mb-4'
     el.setAttribute('data-season', season)
-    
+
     // Probably doesn't work on IE / Edge browsers
     el.innerHTML = `
         <div class="card h-100">
@@ -37,17 +40,17 @@ function renderEpisode(season, episode) {
                 </div>
             </a>
         </div>`
-    
+
     if(season != DEFAULT_SEASON){
         el.style.display = 'none'
     }
-    
+
     document.querySelector('#episodes-container').appendChild(el)
 }
 
 function addSeasonTab(season) {
     // Add the nav tab for this season
-    
+
     let active = ''
     if(DEFAULT_SEASON == season){
         active = 'active'
@@ -57,32 +60,32 @@ function addSeasonTab(season) {
     tab.className += 'nav-item'
     tab.setAttribute('data-season', season)
     tab.innerHTML = `<a class="nav-link ${active}" href="#">${ season }</a>`
-    
+
     tab.onclick = function(e){
         e.preventDefault()
         showSeason(season)
     }
-    
-    
+
+
     document.querySelector('#season-nav').appendChild(tab)
 }
 
 function handleSeason(season) {
     // Handle a season
-    
+
     // Add a tab for this season
     addSeasonTab(season.id)
 
     // Render the episodes
-    // Use reverse to show newest episodes first
-    season.episodes.reverse().map(function(episode){
+    // They are sorted by newest first already
+    season.episodes.map(function(episode){
         renderEpisode(season.id, episode)
     })
 }
 
 function showSeason(season) {
     // Hide all episodes and show the selected season
-    
+
     // Show the selected season episodes
     let episodes = document.querySelectorAll('#episodes-container>div')
     episodes = Array.prototype.slice.apply(episodes)
@@ -93,7 +96,7 @@ function showSeason(season) {
             ep.style.display = 'none'
         }
     })
-    
+
     // Update the selected pill navigation
     let tabs = document.querySelectorAll('#season-nav>li')
     tabs = Array.prototype.slice.apply(tabs)
@@ -105,16 +108,46 @@ function showSeason(season) {
     })
 }
 
-fetch("static/episodes.json")
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(seasons) {
+// Format the date as currently
+const formatDate = raw => {
+  const date = new Date(raw);
+  const day = date.getDate();
+  const month = date.toLocaleString('default', { month: 'short' });
+  const year = date.getFullYear();
+  return `${month} ${day}, ${year}`;
+};
 
-    seasons.map(function(season){
-        handleSeason(season)
-    })
+const groupBySeason = (chapters, chapter) => {
+  // First make sure it's defined
+  chapters[chapter.season] = chapters[chapter.season] || [];
 
-  })
+  // Add the chapter to the episode listt
+  chapters[chapter.season].push({
+    id: Number(chapter.id),
+    title: chapter.title,
+    video: chapter.video,
+    date: formatDate(chapter.date)
+  });
+  return chapters;
+};
 
+// Sort by id DESC
+const byId = (a, b) => b.id - a.id;
+
+drive(SHEET).then(chapters => {
+  // First convert it to an object of { 2019: [...], 2018: [...], ... }
+  const seasons = chapters.reduce(groupBySeason, {});
+
+  // Convert that into an array of [{ id: 2019, episodes: [...] }, ...]
+  // and sort both seasons and chapters by id DESC
+  Object.entries(seasons)
+    .map(([id, episodes]) => ({
+      id: Number(id),
+      episodes: episodes.sort(byId)
+    }))
+    .sort(byId)
+    .forEach(handleSeason);
+});
 
 
 /*
