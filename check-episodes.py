6429d0330e7a929b9b32c7ab183044266eeb48e3
Shane Reustle <shane@reustle.co>
2019-12-22 16:09:26 +0000

    improved broken episode checker

--- check-episodes.py
+++ check-episodes.py
@@ -1,37 +1,68 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 from time import sleep
 from pprint import pprint
+from alive_progress import alive_bar
 
-YT = 'https://www.youtube.com/oembed?format=json&url=http://www.youtube.com/watch?v='
-
-handle = open('_data/episodes.json', 'r')
-contents = handle.read()
-handle.close()
-
-seasons = json.loads(contents)
-
-print('# These episodes are broken:')
-for season in seasons:
-    for episode in season['episodes']:
-        
-        # Skip if no vid
-        if not episode['vid']:
-            continue
-        
-        # Build url
-        path = YT + episode['vid']
-        
-        # Load
-        req = requests.get(path)
-        
-        isValid = True
-        if 'Not Found' in req.content:
-            isValid = False
-        
-            output = '{0}\t{1}\t{2}'.format(episode['title'], episode['date'], episode['vid'])
-            print(output)
-        
-        sleep(1)
+ENDPOINT = 'https://www.youtube.com/oembed?format=json&url=http://www.youtube.com/watch?v='
 
 
+def pull_episode(video_id):
+    """ Pull the json detials of a specific episode """
+  
+    path = ENDPOINT + video_id
+    return requests.get(path)
+
+
+def load_seasons_file():
+    """ Load the episodes file """
+
+    handle = open('static/episodes.json', 'r')
+    contents = handle.read()
+    handle.close()
+
+    return json.loads(contents)
+
+
+def count_episodes(seasons):
+    """ Return the total number of episodes """
+
+    count = 0
+    for season in seasons:
+        count += len(season['episodes'])
+    
+    return count
+
+
+def check_episodes(seasons):
+    """ Check for broken episodes within each season """
+
+
+    ep_count = count_episodes(seasons)
+    
+    # Run the visual bar chart
+    with alive_bar(ep_count, spinner='dots_reverse') as bar:
+        for season in seasons:
+            for episode in season['episodes']:
+                
+                # Skip if no youtube video defined
+                if not episode['vid']:
+                    continue
+                
+                # Load json
+                video_details = pull_episode(episode['vid'])
+                
+                if video_details.status_code != 200:
+                    print(f"{episode['title']}\t\t{episode['date']}\t\t{episode['vid']}")
+                
+                bar()
+                sleep(1)
+
+
+if __name__ == '__main__':
+    print('Checking for broken episodes...')
+    seasons = load_seasons_file()
+    
+    check_episodes(seasons)
